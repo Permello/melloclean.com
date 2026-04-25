32ab57d96593d8e1e58915d16c1e7cae7cecf084
Eduardo Turcios <67609856+Ender-swe@users.noreply.github.com>
2026-04-25 18:52:24 -0500

    chore: removed dead backend code

--- apps/server/migration/backend/app/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2026 Eduardo Turcios. All rights reserved.
-# Unauthorized use, reproduction, or distribution of this file is strictly prohibited.
-from app.utils.config import DATABASE_URL  # noqa: F401, F403
-from app.utils.password import hash_password, verify_password  # noqa: F401
-from app.utils.tokens import generate_token  # noqa: F401
--- apps/server/migration/backend/tests/models/__init__.py
+++
@@ -1,2 +0,0 @@
-# Copyright 2026 Eduardo Turcios. All rights reserved.
-# Unauthorized use, reproduction, or distribution of this file is strictly prohibited.
